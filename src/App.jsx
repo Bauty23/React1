394e9bb577cb694efbf77776fc2c3ece264202d9
Bautista Messina <bautybrune@outlook.com>
2024-07-16 22:37:55 -0300

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+import Card from './components/Card'
+function App() {
+  return (<div className='App'>
+    <h1>Hola react</h1>
+    <div className='container'>
+
+      <Card title="Titulo" description="la descripcion" />
+      <Card title="Titulo" description="la descripcion"/>
+      <Card title="Titulo" description="la descripcion"/>
+      <Card/>
+      <Card/>
+      
+
+    </div>
+  </div>
+  );
+}
+
+export default App
